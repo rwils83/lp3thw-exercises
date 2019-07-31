6727096fe0c7cb5c1f855cd13b67264587bffce8
Ryan Wilson <rwils83@gmail.com>
2019-07-31 00:08:57 -0400

    Added whitespace to Exercise 3 for readability

--- ex3.py
+++ ex3.py
@@ -1,28 +1,41 @@
 # Print statement
 print("I will now count my chickens:")
+
 # 25 + 5(division before addition)
 print("Hens", 25 + 30 / 6)
+
 # 100 - 3 (25 * 3 = 75 %(modulus)4 = 3) Modulus shows the remainder 75/4 is 18 remainer 3
 print("Roosters", 100 - 25 * 3 % 4)
+
 # Print statement
 print("Now I will count the eggs:")
+
 # add 3 + 2 + 1 - 5 + 0 (4%2 before addition) - .25 (division befoere addition) + 6
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
 # Print statement
 print("Is it true that 3 + 2 < 5 - 7?")
+
 # print true or fals is 5 < -2
 print(3 + 2 < 5 - 7)
+
 # print statement and answer
 print("What is 3 + 2?", 3 + 2)
+
 # print statement and answer
 print("What is 5 - 7?", 5 - 7)
+
 # Print statement
 print("Oh, that's why it's False.")
+
 # Print statement
 print("How about some more.")
+
 # print statement and answer: true or false
 print("Is it greater?", 5 > -2)
+
 # print statement and answer: true or false
 print("Is it greater or equal?", 5 >= -2)
+
 # print statement and answer: true or false
 print("Is it less or equal?", 5 <= -2)

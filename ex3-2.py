95e87a5c4e751e105c701b2aaa6599d05ea30ba5
Ryan Wilson <rwils83@gmail.com>
2019-07-31 00:07:27 -0400

    Add Exercise 3-2: Rewrite with float

---
+++ ex3-2.py
@@ -0,0 +1,43 @@
+# Book asked for rewrite to float all answers
+
+# Print statement
+print("I will now count my chickens:")
+
+# 25 + 5(division before addition)
+print("Hens", float(25 + 30 / 6))
+
+# 100 - 3 (25 * 3 = 75 %(modulus)4 = 3) Modulus shows the remainder 75/4 is 18 remainer 3
+print("Roosters", float(100 - 25 * 3 % 4))
+
+# Print statement
+print("Now I will count the eggs:")
+
+# add 3 + 2 + 1 - 5 + 0 (4%2 before addition) - .25 (division befoere addition) + 6
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+# Print statement
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# print true or fals is 5 < -2
+print(float(3 + 2 < 5 - 7))
+
+# print statement and answer
+print("What is 3 + 2?", float(3 + 2))
+
+# print statement and answer
+print("What is 5 - 7?", float(5 - 7))
+
+# Print statement
+print("Oh, that's why it's False.")
+
+# Print statement
+print("How about some more.")
+
+# print statement and answer: true or false
+print("Is it greater?", float(5 > -2))
+
+# print statement and answer: true or false
+print("Is it greater or equal?", float(5 >= -2))
+
+# print statement and answer: true or false
+print("Is it less or equal?", float(5 <= -2))

b82649cc604a2e2f97c49e867f8956cb51342db3
Ryan Wilson <rwils83@gmail.com>
2019-08-21 14:53:57 -0400

    Added ex16 and test file

---
+++ ex16.py
@@ -0,0 +1,36 @@
+from sys import argv
+
+script, filename = argv
+
+print(f'We\'re going to erase {filename}. ' )
+print('If you don\'t want that, hit CTRL-C (^c).')
+print('If you do want that, hit RETURN')
+
+input("?")
+
+print('Opening the file.....')
+
+target = open(filename, 'w')
+
+print('Truncating the file. Goodbye')
+
+target.truncate()
+
+print('I\'m now going to ask you for three lines.')
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+newline = "\n"
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write(newline)
+target.write(line2)
+target.write(newline)
+target.write(line3)
+target.write(newline)
+
+print('And finally we close it')
+target.close()
+
